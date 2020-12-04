2e956bb7c9ec796c03d4aa031de8db939435bad8
Adam Piziak <adpiziak@gmail.com>
2020-12-04 15:23:18 -0500

    Updates

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function createGame() {
-    return axios.post('http://127.0.0.1:5000/create')
-}
-
-export function joinGame(key, name) {
-    return axios.post('http://192.168.1.11:5000/join', {game_key: key, name: name})
-}
