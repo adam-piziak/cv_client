de075593a23044fc9254016d565b1fa0bd2c9e7c
Adam Piziak <adpiziak@gmail.com>
2020-12-04 20:40:02 -0500

    add bots2

--- src/Game.js
+++ src/Game.js
@@ -12,7 +12,7 @@ const RIGHT = 3
 let gGameKey = ""
 let gPlayerKey = ""
 //const socket = io('http://127.0.0.1:5000');
-const socket = io('http://165.22.187.65:5000');
+const socket = io('http://165.22.187.65');
 
 
 class Game extends React.Component {
